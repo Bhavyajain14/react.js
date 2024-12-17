8933036b471a267f7db39e9967c930b88ac3e224
Bhavyajain14 <bhavyajain999478@gmail.com>
2024-12-17 18:47:20 +0530

    Using CSS Modules

--- 10-worldwise/src/component/PageNav.jsx
+++ 10-worldwise/src/component/PageNav.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Homepage</Link>
-      </li>
-      <li>
-        <Link to="/pricing">Pricing</Link>
-      </li>
-      <li>
-        <Link to="/product">Product</Link>
-      </li>
-    </ul>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link to="/">Homepage</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Pricing</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
